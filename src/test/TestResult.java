08218482a1468651b2bb3cc154499307f20a76d5
scarlens250 <74828654+scarlens250@users.noreply.github.com>
2021-02-10 18:03:46 +0300

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,34 @@
 package test;
 
 import domain.Exercise;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class TestResult {
 
     public static void main(String[] args) {
+/**
+ * create variables and array
+ */
+        System.out.println("¬вед≥ть к≥льк≥сть елемент≥в масиву");
+        int k = inputVal('k');
+        int[]a = new int[k];
+        int i = k/2, j = 1, c = k/2;
+        a[i] = 0;
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+
+/**
+ * do operations with massive
+ */
+        for ( i = k/2+1, j = 1, c = k/2-1;c >= 0; i++ , j++ , c-- ) {
+            a[i] = j;
+            a[c] = j;
+        }
+        String s = Arrays.toString(a);
+        System.out.println(s);
+    }
+        private static int inputVal(int variableName) {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
